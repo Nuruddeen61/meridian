614df6f7ab782ca508f60b26bfe639130a5c277f
Nuruddeen Abubakar Abubakar <nuruddeenabubakarabubakar61@gmail.com>
2026-08-29 16:19:19 +0100

    chore: remove invalid Next.js route file

--- apps/web/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export type { StellarNetwork } from "@meridian/stellar-sdk-helpers";
-export type { SupportedStablecoin, ProtocolId } from "@meridian/shared";
-
-export interface WalletState {
-  publicKey: string | null;
-  network: "testnet" | "mainnet";
-  connected: boolean;
-}
